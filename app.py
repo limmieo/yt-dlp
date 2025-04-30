884b19e0cacec3e9792c2d3687ddf246494f9069
Tony Destin <53389706+limmieo@users.noreply.github.com>
2025-04-30 14:23:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import subprocess
 
 app = Flask(__name__)
+CORS(app)  # ✅ Enable CORS for all routes
 
 @app.route('/api', methods=['POST'])
 def download():
